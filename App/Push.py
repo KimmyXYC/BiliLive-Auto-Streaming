e28f6e941b953646fc5a748d17dec3835c7ffca1
KimmyXYC <kimmyxyc@163.com>
2023-02-11 22:19:41 +0800

    feat: Support more push methods feat: Use proxy to send message (Telegram Only) fix #2

--- App/Push.py
+++ App/Push.py
@@ -4,20 +4,65 @@
 # @Software： PyCharm
 # @GitHub: KimmyXYC
 import requests
-from App.Parameter import get_parameter
+from App.Parameter import get_config_file
+
+config_file = get_config_file()
 
 
 def message_push(message):
-    if get_parameter("push", "telegram", "enable"):
+    if config_file["push"]["telegram"]["enable"]:
         telegram(message)
+    if config_file["push"]["pushplus"]["enable"]:
+        pushplus(message)
+    if config_file["push"]["server"]["enable"]:
+        server(message)
+    if config_file["push"]["ijingniu"]["enable"]:
+        ijingniu(message)
 
 
 def telegram(message):
-    bot_token = get_parameter("push", "telegram", "bot_token")
-    group_id = get_parameter("push", "telegram", "group_id")
+    bot_token = config_file["push"]["telegram"]["bot_token"]
+    group_id = config_file["push"]["telegram"]["group_id"]
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={group_id}&text={message}"
-    response = requests.get(url)
+    if config_file["push"]["telegram"]["proxy"]:
+        proxies = {
+            'http': config_file["push"]["telegram"]["proxy"],
+            'https': config_file["push"]["telegram"]["proxy"]
+        }
+        response = requests.get(url, proxies=proxies)
+    else:
+        response = requests.get(url)
     if response.status_code == 200:
         print("【Telegram推送】成功")
     else:
         print("【Telegram推送】失败")
+
+
+def pushplus(message):
+    token = config_file["push"]["pushplus"]["token"]
+    url = f"http://www.pushplus.plus/send/{token}?title=【BiliLive】&text={message}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("【PushPlus推送】成功")
+    else:
+        print("【PushPlus推送】失败")
+
+
+def server(message):
+    sendkey = config_file["push"]["server"]["sendkey"]
+    url = f"f'https://sctapi.ftqq.com/{sendkey}.send?title=【BiliLive】&desp={message}"
+    response = requests.post(url)
+    if response.status_code == 200:
+        print("【Server酱推送】成功")
+    else:
+        print("【Server酱推送】失败")
+
+
+def ijingniu(message):
+    channelkey = config_file["push"]["ijingniu"]["channelkey"]
+    url = f"f'http://push.ijingniu.cn/send?key={channelkey}&head=【BiliLive】&body={message}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("【及时达推送】成功")
+    else:
+        print("【及时达推送】失败")
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 - 安装 `Python3.7` 或更高的版本
 - 执行 `pip install -r requirements.txt` 安装依赖
 - 执行 `python3 login.py` 扫码登录哔哩哔哩账号
-    * 从 `https://api.bilibili.com/x/web-interface/nav` 请求中获取 Cookies 填入也可行
-- 填写 `config.json` 文件
+    * 或从 `https://api.bilibili.com/x/web-interface/nav` 请求中获取 Cookies 填入
+- 填写 `Config.json` 文件
 ```config
 {
     "user_info": {
@@ -14,14 +14,16 @@
     },
     "deploy": {
         "video_path": "video.flv", //视频文件路径（支持相对路径和绝对路径）
-        "live_time": 15 //目标直播时长（单位：秒）
+        "live_time": 2100 //目标直播时长（单位：秒）
     },
     "push": { //推送配置
         "telegram": {
             "enable": false, //是否启用
             "group_id": "",
-            "bot_token": ""
+            "bot_token": "",
+            "proxy": "" //代理地址，支持 socks5 和 http 留空为禁用
         }
+        --snip--
     }
 }
 ```
