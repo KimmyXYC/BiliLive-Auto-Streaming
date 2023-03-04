b26014d9ecb18151e545cc4b1c8d9e48d40fd798
KimmyXYC <kimmyxyc@163.com>
2023-03-04 21:40:53 +0800

    feat: folder path feat: non-stop live broadcast & play video in full fix: #5

--- App/Stream.py
+++ App/Stream.py
@@ -8,25 +8,58 @@ import time
 import subprocess
 from App.Parameter import get_parameter
 
+TYPE = get_parameter("deploy", "type")
 VIDEO_PATH = get_parameter("deploy", "video_path")
 LIVE_TIME = get_parameter("deploy", "live_time")
 
 
 def streaming(live_addr, live_code):
-    start_time = time.time()
-    video_length = get_video_length()
-    end_time = start_time + LIVE_TIME
-    while time.time() + video_length < end_time:
-        ffmpeg_run(live_addr, live_code)
-        time.sleep(3)
-
-    time_left = end_time - time.time()
-    if time_left > 0:
-        cmd = f'ffmpeg -re -i {VIDEO_PATH} -c copy -f flv "{live_addr}{live_code}"'
-        subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        time.sleep(time_left)
+    if TYPE == 1:
+        for root, dirs, files in os.walk(TYPE):
+            if LIVE_TIME == 0:
+                for file in files:
+                    ffmpeg_run(live_addr, live_code, os.path.join(root, file))
+            elif LIVE_TIME == -1:
+                while True:
+                    for file in files:
+                        ffmpeg_run(live_addr, live_code, os.path.join(root, file))
+            else:
+                start_time = time.time()
+                video_length = get_video_length()
+                end_time = start_time + LIVE_TIME
+                while time.time() + video_length < end_time:
+                    for file in files:
+                        ffmpeg_run(live_addr, live_code, os.path.join(root, file))
+                        if time.time() + video_length >= end_time:
+                            break
+                time_left = end_time - time.time()
+                if time_left > 0:
+                    cmd = f'ffmpeg -re -i {VIDEO_PATH} -c copy -f flv "{live_addr}{live_code}"'
+                    subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+                    time.sleep(time_left)
+                else:
+                    pass
     else:
-        pass
+        if LIVE_TIME != 0 and LIVE_TIME != -1:
+            start_time = time.time()
+            video_length = get_video_length()
+            end_time = start_time + LIVE_TIME
+            while time.time() + video_length < end_time:
+                ffmpeg_run(live_addr, live_code, VIDEO_PATH)
+                time.sleep(3)
+
+            time_left = end_time - time.time()
+            if time_left > 0:
+                cmd = f'ffmpeg -re -i {VIDEO_PATH} -c copy -f flv "{live_addr}{live_code}"'
+                subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+                time.sleep(time_left)
+            else:
+                pass
+        elif LIVE_TIME == 0:
+            ffmpeg_run(live_addr, live_code, VIDEO_PATH)
+        else:
+            while True:
+                ffmpeg_run(live_addr, live_code, VIDEO_PATH)
 
 
 def get_video_length():
@@ -37,6 +70,6 @@ def get_video_length():
     return video_length
 
 
-def ffmpeg_run(live_addr, live_code):
+def ffmpeg_run(live_addr, live_code, video_path):
     """推送直播流"""
-    os.system(f'ffmpeg -re -i {VIDEO_PATH} -c copy -f flv "{live_addr}{live_code}"')
+    os.system(f'ffmpeg -re -i {video_path} -c copy -f flv "{live_addr}{live_code}"')
