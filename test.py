8ea639fb8574b7377b5b37ca399902bfcdf9cb2b
エルフです <kimmyxyc@163.com>
2023-05-27 21:34:01 +0800

    fix: Allow User to Test Message Push

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time： 2023/5/27 21:32 
+# @FileName: test.py.py
+# @Software： PyCharm
+# @GitHub: KimmyXYC
+from App.Push import message_push
+
+
+def main():
+    message_push("这是一条测试消息")
+
+
+if __name__ == '__main__':
+    main()
